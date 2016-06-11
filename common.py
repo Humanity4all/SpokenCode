05e2404777d112b0c21c20cf5801481e8dde6ef1
nihlaeth <info@nihlaeth.nl>
2016-06-12 00:52:33 +0200

    Add rule for double-quoted strings

--- common.py
+++ common.py
@@ -6,7 +6,7 @@ from arpeggio import RegExMatch as _
 
 def string():
     """Any string literal."""
-    return [single_quoted_string, double_quoted_string] 
+    return [single_quoted_string, double_quoted_string]
 
 
 def single_quoted_string():
@@ -19,5 +19,19 @@ def single_quoted_string():
             _("[^']")
         ]),
         "'",
+        arpeggio.EOF
+    )
+
+
+def double_quoted_string():
+    """Double quoted string literal."""
+    return (
+        '"',
+        arpeggio.ZeroOrMore([
+            r'\\',
+            r'\"',
+            _('[^"]')
+        ]),
+        '"',
         arpeggio.EOF
     )
--- test_common.py
+++ test_common.py
@@ -4,18 +4,16 @@ from nose.tools import assert_raises
 from SpokenCode import common
 
 
-def parse(instruction):
+def parse(instruction, rule):
     """Parse vim instruction."""
-    parser = arpeggio.ParserPython(
-        common.single_quoted_string,
-        ignore_case=True)
+    parser = arpeggio.ParserPython(rule, ignore_case=True)
     parse_tree = parser.parse(instruction)
 
 
 class Test_single_quoted_string(object):
 
     def test_empty_string(self):
-        parse("''")
+        parse("''", common.single_quoted_string)
 
     def test_valid_string(self):
         for string in [
@@ -25,7 +23,7 @@ class Test_single_quoted_string(object):
                 r"'Say it really isn\\\'t so.'",
                 r"'Say it is not so.\\'",
                 r"'Say it \ is not so.'"]:
-            parse(string)
+            parse(string, common.single_quoted_string)
 
     def test_invalid_string(self):
         for string in [
@@ -36,4 +34,31 @@ class Test_single_quoted_string(object):
                 r"\'Escaped opening quote'",
                 r"'Escaped final quote\'"]:
             with assert_raises(arpeggio.NoMatch):
-                parse(string)
+                parse(string, common.single_quoted_string)
+
+
+class Test_double_quoted_string(object):
+
+    def test_empty_string(self):
+        parse('""', common.double_quoted_string)
+
+    def test_valid_string(self):
+        for string in [
+                '"asdf"',
+                '''"'Hello,' I said. 'How are you?'"''',
+                r'"Say it isn\"t so."',
+                r'"Say it really isn\\\"t so."',
+                r'"Say it is not so.\\"',
+                r'"Say it \ is not so."']:
+            parse(string, common.double_quoted_string)
+
+    def test_invalid_string(self):
+        for string in [
+                '"Missing close quote',
+                'Missing opening quote"',
+                'No quotes',
+                '"Unescaped " quote"',
+                r'\"Escaped opening quote"',
+                r'"Escaped final quote\"']:
+            with assert_raises(arpeggio.NoMatch):
+                parse(string, common.double_quoted_string)
